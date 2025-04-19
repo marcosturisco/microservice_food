a6bb5977a559828eb1daf93456e9fd84342e5d9b
Marcos Turisco <marcosturisco@gmail.com>
2025-04-19 20:22:04 -0300

    The creation of the Microservices and all the migrations

---
+++ pagamentos/src/main/java/br/com/alurafood/pagamentos/http/PedidoClient.java
@@ -0,0 +1,13 @@
+package br.com.alurafood.pagamentos.http;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+
+@FeignClient("pedidos-ms")
+public interface PedidoClient {
+
+    @PutMapping("/pedidos/{id}/pago")
+    void atualizaPagamento(@PathVariable Long id);
+
+}
